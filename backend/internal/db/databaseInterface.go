60a0e66ff63e43969f8623e9beec0e57157919db
Nikolai Åkerholt <nikolai_aakk@hotmail.com>
2020-10-05 12:41:39 +0200

    Initial work on backend

---
+++ backend/internal/db/databaseInterface.go
@@ -0,0 +1,16 @@
+package db
+
+import (
+	"fmt"
+)
+
+// DATABASE Tables
+const (
+
+	// Observer ...
+	TableObserver = "observer"
+	// User ...
+	TableUser = "user"
+	// User ...
+	TableMinigame = "minigame"
+)
