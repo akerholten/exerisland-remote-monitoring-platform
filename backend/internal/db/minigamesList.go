072b8501c9cecb4d03232795fd7cc76dfa3b2539
Nikolai Åkerholt <nikolai_aakk@hotmail.com>
2020-11-02 15:10:13 +0100

    Updated minigames list

--- backend/internal/db/minigamesList.go
+++ backend/internal/db/minigamesList.go
@@ -77,4 +77,142 @@ var minigamesList = []Minigame{
 			},
 		},
 	},
+	Minigame{
+		Id:          "Platform_Minigame",
+		Name:        "Platform Minigame",
+		Description: "Placed on a platform, the player is presented with objects approaching them, some have to be dodged, and some gives the player points when reaching them.",
+		Tags: []string{
+			"Physical", "Cognitive", "Arms", "Hand-eye coordination",
+		},
+		AvailableMetrics: []Metric{
+			Metric{
+				Id:    "Duration",
+				Name:  "Duration",
+				Value: 0,
+				Unit:  "seconds",
+			},
+			Metric{
+				Id:    "Arm_Movement",
+				Name:  "Arm movement",
+				Value: 0,
+				Unit:  "meters",
+			},
+			Metric{
+				Id:    "Leg_Movement",
+				Name:  "Leg movement",
+				Value: 0,
+				Unit:  "meters",
+			},
+			Metric{
+				Id:    "Game_Completion",
+				Name:  "Game completion",
+				Value: 0,
+				Unit:  "true/false",
+			},
+			Metric{
+				Id:    "Obstacle_Not_Dodged",
+				Name:  "Obstacles not dodged",
+				Value: 0,
+				Unit:  "not dodged",
+			},
+			Metric{
+				Id:    "Hittable_Hits",
+				Name:  "Hittable hits",
+				Value: 0,
+				Unit:  "hits",
+			},
+			Metric{
+				Id:    "Score",
+				Name:  "Score",
+				Value: 0,
+				Unit:  "score",
+			},
+		},
+	},
+	Minigame{
+		Id:          "ReactionTimeTrainer_Minigame",
+		Name:        "Reaction Time Trainer",
+		Description: "Based on BATAK reaction time trainer for football-keepers, the reaction time trainer minigame focuses on providing a series of lightbulbs that the player should hit when they light up and make a noise.",
+		Tags: []string{
+			"Physical", "Cognitive", "Arms", "Hand-eye coordination", "Hearing", "Sound",
+		},
+		AvailableMetrics: []Metric{
+			Metric{
+				Id:    "Duration",
+				Name:  "Duration",
+				Value: 0,
+				Unit:  "seconds",
+			},
+			Metric{
+				Id:    "Arm_Movement",
+				Name:  "Arm movement",
+				Value: 0,
+				Unit:  "meters",
+			},
+			Metric{
+				Id:    "Lightbulbs_Hit",
+				Name:  "Lightbulbs hit",
+				Value: 0,
+				Unit:  "hits",
+			},
+			Metric{
+				Id:    "Game_Completion",
+				Name:  "Game completion",
+				Value: 0,
+				Unit:  "true/false",
+			},
+			Metric{
+				Id:    "Score",
+				Name:  "Score",
+				Value: 0,
+				Unit:  "score",
+			},
+		},
+	},
+	Minigame{
+		Id:          "DuckShooter_Minigame",
+		Name:        "Duck Shooter",
+		Description: "The player is presented with a range of drones flying around, the drones has to be shot down and their bullets must be dodged.",
+		Tags: []string{
+			"Physical", "Cognitive", "Arms", "Hand-eye coordination",
+		},
+		AvailableMetrics: []Metric{
+			Metric{
+				Id:    "Duration",
+				Name:  "Duration",
+				Value: 0,
+				Unit:  "seconds",
+			},
+			Metric{
+				Id:    "Arm_Movement",
+				Name:  "Arm movement",
+				Value: 0,
+				Unit:  "meters",
+			},
+			Metric{
+				Id:    "Drones_Hit",
+				Name:  "Drones hit",
+				Value: 0,
+				Unit:  "hits",
+			},
+			Metric{
+				Id:    "Levels_Completed",
+				Name:  "Levels completed",
+				Value: 0,
+				Unit:  "levels",
+			},
+			Metric{
+				Id:    "Game_Completion",
+				Name:  "Game completion",
+				Value: 0,
+				Unit:  "true/false",
+			},
+			Metric{
+				Id:    "Score",
+				Name:  "Score",
+				Value: 0,
+				Unit:  "score",
+			},
+		},
+	},
 }
